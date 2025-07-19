aa3b9441d537af325eefbbcb0d46ef850736fb16
bernasrodrigues <bernardokiko@gmail.com>
2025-07-19 01:16:49 +0100

    added option to full page click

--- GUI/ChoosePage.py
+++ GUI/ChoosePage.py
@@ -39,7 +39,9 @@ class ChoosePage(tk.Frame):
         self.canvas_degrade = self.canvas.create_image(1080 / 2, 1980 / 2,
                                                        anchor=tk.CENTER,
                                                        image=self.controller.degrade)
-        self.canvas.tag_bind(self.canvas_degrade, '<Button-1>', lambda event: self.clickedArea(event))  # checks the area where it was clicked
+
+        #self.canvas.tag_bind(self.canvas_degrade, '<Button-1>', lambda event: self.controller.show_frame( "CompPage"))  # a click anywhere on the image will go to the next page
+        self.canvas.tag_bind(self.canvas_degrade, '<Button-1>', lambda event: self.clickedArea(event))  # check the area where it is clicked and goes to the next page
 
         ### BUTTONS ###
         # Middle Button
